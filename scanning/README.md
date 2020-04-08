47b30dd9e73fd80dd86e308a3af0096e087212b6
pang <pang@zhaw.ch>
2020-04-08 10:57:33 +0200

    updated scanning

--- scanning/README.md
+++ scanning/README.md
@@ -1,17 +1,65 @@
 # Image Scanner
 
 Wraps around `docker pull` and scans an image with Clair. Will print the image
-tag and return code of the scan. The scan succeeds when no vulnerability of
-'high' severity or higher is found.
+tag and the highest CVE severity level found in the image.
 
 ## Prerequisites
 
-- Install Docker
+- Install Docker and docker-compose
 - `pip install -r requirements.txt`
+
+## Setup
+
+Run clair:
+```
+cd clair-test
+sudo docker-compose up -d
+```
+Keep in mind that clair takes a while to populate the CVE database the first time,
+and during that time it will return no vulnerabilities. This takes 20-30 minutes.
 
 ## Usage
 
-Example for the `ubuntu:latest` image:
+The program has two modes, 'experiment' and 'pull'.
+
+### Pull mode
+
+Pull mode will pull the image to the local registry and scan it with clair.
+It will print the 'message' that will be sent to the policy engine.
+
+Example:
 ```
-python scan.py ubuntu:latest
+$ python3 scan.py pull nginx:latest
+
+...
+
+Image: nginx:latest, Result: Low
 ```
+The severity levels for clair are:
+- Unknown
+- Negligible
+- Low
+- Medium
+- High
+- Critical
+- Defcon1
+
+Refer to clair's documentation for a detailed explanation of the severity levels.
+The program will return 'None' if no vlunerabilities are found by clair.
+
+### Experiment mode
+
+Experiment mode will scan all images of the official `library` registry to
+obtain an overview of the security status of official images.
+
+Be aware that this
+mode will use a large amount of storage and despite cleaning up, some data will
+remain. As of now, it can consume ~80 GB.
+
+ Here are some sample results, limited to 'Low' vulnerabilities or higher.
+
+- Total images: 160
+- Images scanned successfully: 148
+- At least one Low: 108 (73%)
+- At least one Medium: 39 (26%)
+- At least one High: 6 (4%)
--- scanning/scan.py
+++ scanning/scan.py
@@ -1,17 +1,105 @@
-import sys
+import requests
+import json
 import subprocess
-import docker
+import sys
 
 
-client = docker.from_env()
+def get_names():
+    link = 'https://hub.docker.com/v2/repositories/library/'
+    names = []
+    while True:
+        data = json.loads(requests.get(link).text)
+        if data['next'] is None:
+            break
+        link = data['next']
+        for item in data['results']:
+            names.append(item['name'])
+    print(names)
+    return names
 
 
-def pull(image):
-    return client.images.pull(image)
+def cleanup(names):
+    for image in names:
+        subprocess.call(f'docker rmi {image}', shell=True)
+
+
+def scan(image, tag):
+    subprocess.call(f'docker pull {image}:{tag}', shell=True)
+    subprocess.call(f'docker tag {image} localhost:5000/{image}-test', shell=True)
+    subprocess.call(f'docker push localhost:5000/{image}-test', shell=True)
+    p = subprocess.run(f'CLAIR_ADDR=http://localhost:6060 CLAIR_THRESHOLD=10 REGISTRY_INSECURE=TRUE JSON_OUTPUT=TRUE klar localhost:5000/{image}-test', shell=True, stdout=subprocess.PIPE)
+    return json.loads(p.stdout)
+
+
+def scan_all(names):
+    results = {
+        "Unknown": 0,
+        "Negligible": 0,
+        "Low": 0,
+        "Medium": 0,
+        "High": 0,
+        "Critical": 0,
+        "Defcon1": 0
+    }
+    errors = 0
+    for image in names:
+        try:
+            result = scan(image, 'latest')
+            if 'Unknown' in result['Vulnerabilities']:
+                results['Unknown'] += 1
+            if 'Negligible' in result['Vulnerabilities']:
+                results['Negligible'] += 1
+            if 'Low' in result['Vulnerabilities']:
+                results['Low'] += 1
+            if 'Medium' in result['Vulnerabilities']:
+                results['Medium'] += 1
+            if 'High' in result['Vulnerabilities']:
+                results['High'] += 1
+            if 'Critical' in result['Vulnerabilities']:
+                results['Critical'] += 1
+            if 'Defcon1' in result['Vulnerabilities']:
+                results['Defcon1'] += 1
+        except:
+            errors += 1
+    print(results)
+    print (f'Errors: {errors}')
+    print(len(names))
+
+
+def check(image, tag):
+    result = scan(image, tag)
+    if 'Defcon1' in result['Vulnerabilities']:
+        return 'Defcon1'
+    elif 'Critical' in result['Vulnerabilities']:
+        return 'Critical'
+    elif 'High' in result['Vulnerabilities']:
+        return 'High'
+    elif 'Medium' in result['Vulnerabilities']:
+        return 'Medium'
+    elif 'Low' in result['Vulnerabilities']:
+        return 'Low'
+    elif 'Negligible' in result['Vulnerabilities']:
+        return 'Negligible'
+    elif 'Unknown' in result['Vulnerabilities']:
+        return 'Unknown'
 
 
 if __name__ == '__main__':
-    image = pull(sys.argv[1])
-    print(image.tags)
-    status = subprocess.call(f'curl -s -L https://raw.githubusercontent.com/simonsdave/clair-cicd/master/bin/assess-image-risk.sh | bash -s -- {image.tags[0]}', shell=True)
-    print(status)
+    if len(sys.argv) < 2:
+        print("Usage: scan.py <experiment/pull> <image>")
+    else:
+        if sys.argv[1] == 'experiment':
+            while True:
+                flag = input('This option will use a lot of storage and some data cannot be automatically cleaned up. Continue? [y/n]')
+                if flag == 'y':
+                    names = get_names()
+                    scan_all(names)
+                    cleanup(names)
+                    break
+                elif flag == 'n':
+                    break
+        elif sys.argv[1] == 'pull' and len(sys.argv) == 3:
+            result = check(sys.argv[2].split(':')[0], sys.argv[2].split(':')[1])
+            print(f'Image: {sys.argv[2]}, Result: {result}')
+        else:
+            print("Usage: scan.py <experiment/pull> <image>")
