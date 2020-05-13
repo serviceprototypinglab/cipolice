8d3e588c346fdcbfb53030d80e93525574584562
spio@tougener <spio@tougener>
2020-05-13 12:22:57 +0200

    - more complete dockerisation including docker-in-docker and oc client - implementation of AMQP (RabbitMQ) listener - adaptive deactivation of AMQP and HTTP listeners depending on the availability of modules

--- src/cipolice.py
+++ src/cipolice.py
@@ -2,11 +2,20 @@ import argparse
 import sys
 import json
 
-import flask
+try:
+    import flask
+except:
+    print("(Warning: no web mode due to missing flask.)")
+
+try:
+    import pika
+except:
+    print("(Warning: no message queue mode due to missing pika.)")
 
 import xrules
 
-app = flask.Flask(__name__)
+if "flask" in globals():
+    app = flask.Flask(__name__)
 
 web_ruleset = None
 
@@ -14,18 +23,31 @@ def runrules(msg, ruleset):
     r = xrules.makerules(ruleset)
     xrules.applyrules(r, msg, globals())
 
-def selftest(msg, ruleset):
-    runrules(msg, ruleset)
+if "flask" in globals():
+    @app.route('/', methods=['POST'])
+    def webhook():
+        msg = flask.request.get_json()
+        if msg == None:
+            data = list(flask.request.form.keys())[0]
+            msg = json.loads(data)
+        runrules(msg, web_ruleset)
 
-@app.route('/', methods=['POST'])
-def webhook():
-    msg = flask.request.get_json()
-    if msg == None:
-        data = list(flask.request.form.keys())[0]
-        msg = json.loads(data)
-    runrules(msg, web_ruleset)
+        return "OK"
 
-    return "OK"
+def queueloop(ruleset):
+    print(" * Listening to AMQP...")
+
+    connection = pika.BlockingConnection()
+    channel = connection.channel()
+
+    for method_frame, properties, body in channel.consume("hello"):
+        #print(method_frame, properties, body, type(body))
+        msg = json.loads(body.decode())
+        channel.basic_ack(method_frame.delivery_tag)
+        runrules(msg, ruleset)
+
+    #requeued_messages = channel.cancel()
+    #connection.close()
 
 def main():
     global web_ruleset
@@ -33,8 +55,10 @@ def main():
     ruleset_default = "cipolice-example.rules"
 
     parser = argparse.ArgumentParser(description="CIPolicE")
-    parser.add_argument("-w", "--web", action="store_true", help="Run as web service on port 10080.")
-    parser.add_argument("-m", "--mq", action="store_true", help="Run as message queue client on port XXX.")
+    if "flask" in globals():
+        parser.add_argument("-w", "--web", action="store_true", help="Run as web service on port 10080.")
+    if "pika" in globals():
+        parser.add_argument("-m", "--mq", action="store_true", help="Run as message queue client on port XXX.")
     parser.add_argument("-t", "--test", action="store_true", help="Run in self-test mode.")
     parser.add_argument("-r", "--ruleset", action="store", default=ruleset_default, help=f"Ruleset to use (default: {ruleset_default}).")
     parser.add_argument("msg", nargs="?")
@@ -44,15 +68,14 @@ def main():
         web_ruleset = args.ruleset
         app.run(host="0.0.0.0", port=10080)
     elif args.mq:
-        print("NOT IMPLEMENTED YET")
-        exit(-1)
+        queueloop(args.ruleset)
     elif args.test:
         msg = args.msg
         if not msg:
             msg = {"cve": "CVE-2020-7050", "clairresult": -1, "image": "node:12", "compromised": True}
         else:
             msg = json.loads(msg)
-        selftest(msg, args.ruleset)
+        runrules(msg, args.ruleset)
     else:
         print("No operation specified! Run with -h.", file=sys.stderr)
         exit(-1)
