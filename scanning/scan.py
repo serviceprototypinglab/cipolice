61eb2f55a0640a5b0acaa329cd96f21c0026dbf7
pang <pang@zhaw.ch>
2020-05-18 13:48:44 +0200

    added oc scan, push wrapper, timestamp annotation, avg til

--- scanning/scan.py
+++ scanning/scan.py
@@ -4,6 +4,8 @@ import json
 import subprocess
 import sys
 import find
+import calendar
+import time
 
 
 def get_names():
@@ -25,21 +27,30 @@ def cleanup(names):
         subprocess.call(f'docker rmi {image}', shell=True)
 
 
-def scan(image, tag):
-    subprocess.call(f'docker pull {image}:{tag}', shell=True)
+def oc_find():
+    b = subprocess.run('oc get all -o json', shell=True, stdout=subprocess.PIPE)
+    return find.extract_values(json.loads(b.stdout), 'image')
+
+
+def push(image, tag):
     subprocess.call(f'docker tag {image} localhost:5000/{image}-test', shell=True)
     subprocess.call(f'docker push localhost:5000/{image}-test', shell=True)
     p = subprocess.run(f'CLAIR_ADDR=http://localhost:6060 CLAIR_THRESHOLD=10 REGISTRY_INSECURE=TRUE JSON_OUTPUT=TRUE klar localhost:5000/{image}-test', shell=True, stdout=subprocess.PIPE)
+    return json.loads(p.stdout)
+
+
+def pull(image, tag):
+    subprocess.call(f'docker pull {image}:{tag}', shell=True)
+    p = push(image, tag)
     try:
-        b = subprocess.run('oc get all -o json', shell=True, stdout=subprocess.PIPE)
-        images = find.extract_values(json.loads(b.stdout), 'image')
+        images = oc_find()
         if f'{image}:{tag}' in images:
             print(f"Warning, image {image} is currently in use")
         else:
             print("Scanned image has not been deployed to the cluster")
     except:
         print("Warning, cannot check cluster status")
-    return json.loads(p.stdout)
+    return p
 
 
 def scan_all(names):
@@ -52,52 +63,87 @@ def scan_all(names):
         "Critical": 0,
         "Defcon1": 0
     }
+    results_avg = {}
     errors = 0
     for image in names:
         try:
-            result = scan(image, 'latest')
-            if 'Unknown' in result['Vulnerabilities']:
+            result = check(image, 'latest','pull')
+            if result[0] == 1:
                 results['Unknown'] += 1
-            if 'Negligible' in result['Vulnerabilities']:
+            if result[0] == 2:
                 results['Negligible'] += 1
-            if 'Low' in result['Vulnerabilities']:
+            if result[0] == 3:
                 results['Low'] += 1
-            if 'Medium' in result['Vulnerabilities']:
+            if result[0] == 4:
                 results['Medium'] += 1
-            if 'High' in result['Vulnerabilities']:
+            if result[0] == 5:
                 results['High'] += 1
-            if 'Critical' in result['Vulnerabilities']:
+            if result[0] == 6:
                 results['Critical'] += 1
-            if 'Defcon1' in result['Vulnerabilities']:
+            if result[0] == 7:
                 results['Defcon1'] += 1
+            results_avg[image] = result[1]
         except:
             errors += 1
     print(results)
+    print(results_avg)
     print (f'Errors: {errors}')
     print(len(names))
 
 
-def check(image, tag):
-    result = scan(image, tag)
+
+def check(image, tag, mode):
+    if mode == 'pull':
+        result = pull(image, tag)
+    elif mode == 'push':
+        result = push(image, tag)
+    tilmax = 0
+    tilavg = 0
     if 'Defcon1' in result['Vulnerabilities']:
-        return 7
+        tilmax = 7
     elif 'Critical' in result['Vulnerabilities']:
-        return 6
+        tilmax = 6
     elif 'High' in result['Vulnerabilities']:
-        return 5
+        tilmax = 5
     elif 'Medium' in result['Vulnerabilities']:
-        return 4
+        tilmax = 4
     elif 'Low' in result['Vulnerabilities']:
-        return 3
+        tilmax = 3
     elif 'Negligible' in result['Vulnerabilities']:
-        return 2
+        tilmax = 2
     elif 'Unknown' in result['Vulnerabilities']:
-        return 1
+        tilmax = 1
     else:
-        return 0
+        tilmax = 0
+    sum = 0
+    count = 0
+    if 'Defcon1' in result['Vulnerabilities']:
+        sum += len(result['Vulnerabilities']['Defcon1'])*7
+        count += len(result['Vulnerabilities']['Defcon1'])
+    if 'Critical' in result['Vulnerabilities']:
+        sum += len(result['Vulnerabilities']['Critical'])*6
+        count += len(result['Vulnerabilities']['Critical'])
+    if 'High' in result['Vulnerabilities']:
+        sum += len(result['Vulnerabilities']['High'])*5
+        count += len(result['Vulnerabilities']['High'])
+    if 'Medium' in result['Vulnerabilities']:
+        sum += len(result['Vulnerabilities']['Medium'])*4
+        count += len(result['Vulnerabilities']['Medium'])
+    if 'Low' in result['Vulnerabilities']:
+        sum += len(result['Vulnerabilities']['Low'])*3
+        count += len(result['Vulnerabilities']['Low'])
+    if 'Negligible' in result['Vulnerabilities']:
+        sum += len(result['Vulnerabilities']['Negligible'])*2
+        count += len(result['Vulnerabilities']['Negligible'])
+    if 'Unknown' in result['Vulnerabilities']:
+        sum += len(result['Vulnerabilities']['Unknown'])
+        count += len(result['Vulnerabilities']['Unknown'])
+    tilavg = sum/count
+    return [tilmax, tilavg]
+
 
 def detail(image, tag):
-    result = scan(image, tag)
+    result = pull(image, tag)
     print(json.dumps(result['Vulnerabilities'], indent=2))
     with open('output.json', 'w') as f:
         json.dump(result['Vulnerabilities'], f, indent=2)
@@ -109,6 +155,7 @@ def detail(image, tag):
         ref = data['image']['dockerImageReference'].split('@')[1]
         print(ref)
         subprocess.call(f'oc annotate images/{ref} images.openshift.io/deny-execution=false --overwrite --as system:admin', shell=True)
+        subprocess.call(f'oc annotate images/{ref} images.openshift.io/timestamp={calendar.timegm(time.gmtime())} --overwrite --as system:admin', shell=True)
 
 
 
@@ -126,8 +173,11 @@ if __name__ == '__main__':
                     break
                 elif flag == 'n':
                     break
+        elif sys.argv[1] == 'cluster':
+            names = oc_find()
+            scan_all(names)
         elif sys.argv[1] == 'pull' and len(sys.argv) == 3:
-            result = check(sys.argv[2].split(':')[0], sys.argv[2].split(':')[1])
+            result = check(sys.argv[2].split(':')[0], sys.argv[2].split(':')[1], 'pull')
             """
             message = f'{{"image": "{sys.argv[2]}", "level": {result}}}'
             print(message)
@@ -139,9 +189,25 @@ if __name__ == '__main__':
                               body=message)
             connection.close()
             """
-            message = {"image": sys.argv[2], "level": result}
+            message = {"image": sys.argv[2], "level": result[0], "avg": result[1]}
             print(message)
-            requests.post('http://localhost:10080', json=message)
+            #requests.post('http://localhost:10080', json=message)
+        elif sys.argv[1] == 'push' and len(sys.argv) == 3:
+            result = check(sys.argv[2].split(':')[0], sys.argv[2].split(':')[1], 'push')
+            """
+            message = f'{{"image": "{sys.argv[2]}", "level": {result}}}'
+            print(message)
+            connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+            channel = connection.channel()
+            channel.queue_declare(queue='hello')
+            channel.basic_publish(exchange='',
+                              routing_key='hello',
+                              body=message)
+            connection.close()
+            """
+            message = {"image": sys.argv[2], "level": result[0], "avg": result[1]}
+            print(message)
+            #requests.post('http://localhost:10080', json=message)
         elif sys.argv[1] == 'detail' and len(sys.argv) == 3:
             detail(sys.argv[2].split(':')[0], sys.argv[2].split(':')[1])
 
